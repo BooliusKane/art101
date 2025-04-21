36766bdda8d94f728e59de09d8740307534aa514
BooliusKane <juliusgomez2002@gmail.com>
2025-04-21 15:12:12 -0700

    Pseudocode Lab
    Adding comments to lab for js file and copying that code into html file for organization.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Creating pseudocode list.
+ * 
+ * Pseudocoding adding oil to car and coding rock paper scissors game.
+ * 
+ * Julius Gomez
+ * 4/21/2025
+ */
+ 
+/**
+ * Adding Oil to Car:
+      Open hood
+      Check what oil is needed
+      Check the dipstick for how much oil you need
+      Gather funnel and oil, and paper towel
+      Add in Oil
+      Wipe off dipstick
+      Check dipstick again
+      Close hood
+ */
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+/**
+ * Rock, paper, scissors:
+      Create all three icons
+      Collect user input
+      Validate the input
+      Collect other users input
+      Validate that input too
+      Test win, draw, lose options
+*/
